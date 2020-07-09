f5ffffe2b24fa172504adf239203489f0836ff16
LiuTuodong <68057024+LiuTuodong@users.noreply.github.com>
2020-07-09 18:16:18 +0800

    Add files via upload

---
+++ 1.py
@@ -0,0 +1,14 @@
+class Calculator:
+    name='Good calculator'
+    price=18
+    def add(self,x,y):
+        print(self.name)
+        result=x+y
+        print(result)
+    def minus(self,x,y):
+        redult=x-y
+        print(result)
+    def times(self,x,y):
+        print(x*y)
+    def divide(self,x,y):
+        print(x/y)
